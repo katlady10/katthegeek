b0c9ddd66c8618df487c842ac2a42dd33c5d1416
Kat <katlady10>
2013-04-07 13:45:19 -0600

    added console.log for the nerds out there

--- _assets/js/main-min.js
+++ _assets/js/main-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".fancybox").fancybox({openEffect:"none",closeEffect:"none"})});
+$(document).ready(function(){$(".fancybox").fancybox({openEffect:"none",closeEffect:"none"});console.log("Welcome Nerds! I see that you're taking a look at my code. The blog is on wordpress...so don't judge too harshly! Hope you have a great day - geek on!")});
--- _assets/js/main.js
+++ _assets/js/main.js
@@ -3,4 +3,5 @@ $(document).ready(function() {
 		openEffect	: 'none',
 		closeEffect	: 'none'
 	});
+	console.log("Welcome Nerds! I see that you're taking a look at my code. The blog is on wordpress...so don't judge too harshly! Hope you have a great day - geek on!");
 });
